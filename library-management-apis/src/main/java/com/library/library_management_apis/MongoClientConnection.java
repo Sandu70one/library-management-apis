cd2221e0493fd614ff2c254027a59c51e9485296
Sandu70one <sanduaruna70one@gmail.com>
2025-11-08 07:34:22 +0530

    initialized the spring project connected to the the mongodb

---
+++ library-management-apis/src/main/java/com/library/library_management_apis/MongoClientConnection.java
@@ -0,0 +1,39 @@
+package com.library.library_management_apis;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class MongoClientConnection {
+    public static void main(String[] args) {
+        String connectionString = System.getenv("MONGODB_URI");
+        if (connectionString == null || connectionString.isEmpty()) {
+            System.err.println("Set MONGODB_URI environment variable (do not commit credentials).");
+            return;
+        }
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
+            try {
+                MongoDatabase database = mongoClient.getDatabase("admin");
+                database.runCommand(new Document("ping", 1));
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+            } catch (MongoException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ library-management-apis/src/test/java/com/library/library_management_apis/LibraryManagementApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library_management_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagementApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
